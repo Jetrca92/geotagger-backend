d2ed56cec43dde9859f647635e6b1d86233ab1a4
Jetrca92 <blaz.guzej@gmail.com>
2024-11-14 07:05:01 +0100

    Add update location

--- src/modules/location/location.controller.ts
+++ src/modules/location/location.controller.ts
@@ -1,26 +1,56 @@
-import { Body, Controller, Get, Post } from '@nestjs/common'
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Patch, Post, UseGuards } from '@nestjs/common'
 import { MapsService } from 'modules/maps/maps.service'
 import { LocationService } from './location.service'
 import { GetCurrentUserById } from 'utils/get-user-by-id.decorator'
 import { CreateLocationDto } from './dto/create-location.dto'
-import { ApiOperation, ApiResponse } from '@nestjs/swagger'
+import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger'
 import { LocationDto } from './dto/location.dto'
+import { DatabaseService } from 'modules/database/database.service'
+import { UpdateLocationDto } from './dto/update-location.dto'
+import { AuthGuard } from '@nestjs/passport'
 
 @Controller('location')
 export class LocationController {
   constructor(
     private readonly googleMapsService: MapsService,
     private readonly locationService: LocationService,
+    private prisma: DatabaseService,
   ) {}
 
   @ApiOperation({ summary: 'Return list of latest locations' })
   @ApiResponse({ status: 200, description: 'List of latest locations', type: LocationDto })
   @Get('')
+  @HttpCode(HttpStatus.OK)
+  async getLocations(): Promise<LocationDto[]> {
+    return this.prisma.location.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+    })
+  }
+
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Create a new location' })
+  @ApiResponse({ status: 201, description: 'Location successfully created' })
+  @ApiResponse({ status: 401, description: 'Unauthorized.' })
   @Post('')
   async addLocation(
     @GetCurrentUserById() userId: string,
     @Body() locationDto: CreateLocationDto,
   ): Promise<LocationDto> {
     return this.locationService.createLocation(locationDto, userId)
+  }
+
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Update location information' })
+  @ApiResponse({ status: 200, description: 'Updated location' })
+  @UseGuards(AuthGuard('jwt'))
+  @Patch('/:id')
+  async updateLocation(
+    @Param('id') locationId: string,
+    @GetCurrentUserById() userId: string,
+    updateLocationDto: UpdateLocationDto,
+  ): Promise<LocationDto> {
+    return this.locationService.updateLocation(userId, locationId, updateLocationDto)
   }
 }
--- src/modules/location/location.service.ts
+++ src/modules/location/location.service.ts
@@ -1,13 +1,20 @@
-import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common'
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  UnauthorizedException,
+} from '@nestjs/common'
 import { DatabaseService } from 'modules/database/database.service'
 import { CreateLocationDto } from './dto/create-location.dto'
 import { LocationDto } from './dto/location.dto'
+import { UpdateLocationDto } from './dto/update-location.dto'
 
 @Injectable()
 export class LocationService {
   constructor(private readonly prisma: DatabaseService) {}
 
-  async createLocation(locationDto: CreateLocationDto, userId: string) {
+  async createLocation(locationDto: CreateLocationDto, userId: string): Promise<LocationDto> {
     try {
       const newLocation = await this.prisma.location.create({
         data: {
@@ -24,5 +31,37 @@ export class LocationService {
       Logger.error(error)
       throw new InternalServerErrorException('Failed to create location')
     }
+  }
+
+  async updateLocation(userId: string, locationId: string, updateLocationDto: UpdateLocationDto): Promise<LocationDto> {
+    const location = (await this.prisma.location.findUnique({ where: { id: locationId } })) as LocationDto
+
+    if (!location) {
+      Logger.warn('Location not found')
+      throw new BadRequestException('Location not found')
+    }
+
+    if (location.ownerId !== userId) {
+      Logger.warn('You are not the owner of location')
+      throw new UnauthorizedException('You are not the owner.')
+    }
+
+    const updates: Partial<LocationDto> = {}
+    if (updateLocationDto.address) updates.address = updateLocationDto.address
+    if (updateLocationDto.imageUrl) updates.imageUrl = updateLocationDto.imageUrl
+    if (updateLocationDto.latitude) updates.latitude = updateLocationDto.latitude
+    if (updateLocationDto.longitude) updates.longitude = updateLocationDto.longitude
+
+    if (Object.keys(updates).length === 0) {
+      Logger.warn('No fields to update.')
+      throw new BadRequestException('No fields to update.')
+    }
+
+    Logger.log(`Location updated successfully for location`)
+    return this.prisma.location.update({
+      where: { id: locationId },
+      data: updates,
+      select: { latitude: true, longitude: true, imageUrl: true, address: true, ownerId: true },
+    })
   }
 }
